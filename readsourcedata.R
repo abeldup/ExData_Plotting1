107af4a04bf24a4aa8b2d64f28e02b8fed26853f
Abel du Plessis <abel.duplessis@gmail.com>
2015-11-04 14:09:57 +0200

    Removed need for dplyr from readsourcedata.R

--- readsourcedata.R
+++ readsourcedata.R
@@ -13,7 +13,12 @@ readSourceData <- function(prows = NA) {
   alldata$Date <- as.Date(alldata$Date, format="%d/%m/%Y")
   ##Remove the time string column
   alldata$Time <- NULL
+  ##Reduce the data to the two dates
+  alldata <- subset(alldata, Date >= as.Date("2007-02-01", format="%Y-%m-%d") & Date <= as.Date("2007-02-02", format="%Y-%m-%d"))
   ##Get the columns in the desired order
-  alldata <- select(alldata, DateTime, Date:Sub_metering_3)
+  alldata <- subset(alldata, 
+                    select = c(DateTime, Date, Global_active_power, Global_reactive_power, 
+                               Voltage, Global_intensity, Sub_metering_1, 
+                               Sub_metering_2, Sub_metering_3))
   return(na.omit(alldata))
 }
